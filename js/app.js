d960107259ac6436935bae792fafcaaa995982de
Aurelia Moser <auremoser@gmail.com>
2014-05-04 17:00:38 +0200

    fix dates ftw

--- js/app.js
+++ js/app.js
@@ -19,19 +19,28 @@ $(document).ready(function() {
                 posts = posts.concat(data.mostViewed);
             }
             //create place holders for our HTML output
-            var $content, $header, $date;
+            var $content, $header, $date, $link;
             //iterate over all editors picks,
             $.each(posts, function(i, post){
                 console.log(post);
                 //create the HTML containers, following the template provided.
                 $content = $('<div class="small-offset-1 small-10 columns items">');
+                $link = $('<a>').html(post.webTitle).attr('href', post.webUrl).attr('target','_blank');
                 $header = $('<h1 class="itemheadlinetext">');
                 $date = $('<span class="metatext">');
-                $header.html(post.webTitle);
-                $date.html(post.webPublicationDate);
+                $header.html($link);
+                $date.html(_formatDate(post.webPublicationDate));
                 $content.append($header).append($date);
                 $('.media-container').append($content);
             });
         }
     });
+ 
+function _formatDate(str){
+    var date = new Date(str),
+        yyyy = date.getFullYear().toString();
+        mm = (date.getMonth()+1).toString(); // getMonth() is zero-based
+        dd  = date.getDate().toString();
+    return (dd[1]?dd:"0"+dd[0]) + '/' + (mm[1]?mm:"0"+mm[0]) + '/' + yyyy;
+}
 });
